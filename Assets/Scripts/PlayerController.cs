72483bb2c181e7c13e814d389b774a544cb71cf1
Rafael Lima <95830415+rafahgol@users.noreply.github.com>
2025-01-08 08:53:12 -0300

    dash push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,6 +13,16 @@ public class PlayerController : MonoBehaviour
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
+    // Dash ↓↓↓↓↓↓ 
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashingPower = 50f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    [SerializeField] private TrailRenderer tr;
+
+    // Dash ↑↑↑↑↑↑
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,14 +30,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Move();
-        Jump();
+        DashLock();
+        Dash();
+        if (isDashing == false)
+        {
+            Move();
+            Jump();
+        }
+
     }
 
     void Move()
     {
         float moveInput = Input.GetAxis("Horizontal"); // "A/D" ou "Setas" padrão
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        print(rb.linearVelocity);
     }
 
     void Jump()
@@ -47,4 +66,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Lida com o input do Dash. Posto aqui pra ficar mais organizado
+    void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(DashRT());
+        }
+    }
+
+    //Para prevenir o player de se mexer, pular ou algo do tipo enquanto dá dash.
+    void DashLock()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+    }
+
+    //Rotina que trata o efeito do dash
+    private IEnumerator DashRT()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
 }
