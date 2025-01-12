d0cee4c4d1c36ae4deac25d5541cb6fc8cfb11fb
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2025-01-11 21:29:10 -0300

    finalizei os prototipos do ataque do jester

---
+++ Assets/Scripts/Jester/OurosSpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class OurosSpawner : MonoBehaviour
+{
+    public GameObject ourosPrefab;
+    public Transform spawnArea;
+    public float spawnHeight = 1.5f;
+    public float positionVariance = 1.5f;
+    public float antecipationDuration = 2f;
+    public float attackDuration = 5f;
+    public float spawnInterval = 0.1f;
+    public bool isSpawning = false;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            if(isSpawning) {
+                EndSpawning();
+                return;
+            }
+
+            StartSpawning();
+        }
+    }
+
+    public void StartSpawning()
+    {
+        isSpawning = true;
+        InvokeRepeating(nameof(SpawnOuros), antecipationDuration, spawnInterval);
+    }
+
+    void SpawnOuros()
+    {
+        Vector3 posicaoCentro = spawnArea.position;
+        Vector3 dimensoes = spawnArea.localScale / 2;
+
+        float posicaoX = Random.Range(-dimensoes.x, dimensoes.x);
+
+        Vector3 posicaoAleatoria = new Vector3(posicaoCentro.x + posicaoX, posicaoCentro.y, posicaoCentro.z);
+        Instantiate(ourosPrefab, posicaoAleatoria, ourosPrefab.transform.rotation);
+    }
+
+    public void EndSpawning()
+    {
+        StartCoroutine(WaitSpawning(attackDuration));
+    }
+
+    IEnumerator WaitSpawning(float attackDuration)
+    {
+        yield return new WaitForSeconds(attackDuration);
+        CancelInvoke(nameof(SpawnOuros));
+        isSpawning = false;
+    }
+}
