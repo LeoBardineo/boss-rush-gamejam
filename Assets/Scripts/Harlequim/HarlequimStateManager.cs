c7f5ef3e678cca1ffce1cff401092147be0f794c
Rafael Lima <95830415+rafahgol@users.noreply.github.com>
2025-01-26 22:00:22 -0300

    ataque palma

--- Assets/Scripts/Harlequim/HarlequimStateManager.cs
+++ Assets/Scripts/Harlequim/HarlequimStateManager.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class HarlequimStateManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    StateManager stateManager;
+    public float idleDuration;
+
     void Start()
     {
-        
+        stateManager = new StateManager();
+        stateManager.Start(new JesterIdleState(gameObject));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        stateManager.Update();
     }
 }
---
+++ Assets/Scripts/Harlequim/States/ClappingState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ClappingState : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
