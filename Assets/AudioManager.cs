e4c1939e76f00c21ba760335115e621ae2b9de30
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2025-02-08 03:15:53 -0300

    botoes com som, mixer, audiomanager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private void Start()
+    {
+        float musicVol = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        float sfxVol = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        musicSlider.value = musicVol;
+        sfxSlider.value = sfxVol;
+
+        SetMusicVolume(musicVol);
+        SetSFXVolume(sfxVol);
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        float dB = volume > 0f ? Mathf.Log10(volume) * 20 : -80f;
+        audioMixer.SetFloat("MusicVolume", dB);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        float dB = volume > 0f ? Mathf.Log10(volume) * 20 : -80f;
+        audioMixer.SetFloat("SFXVolume", dB);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+}
